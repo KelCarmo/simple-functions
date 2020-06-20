cdc6dee741ef80628e9583006be07a27f3ad0bb6
Kelvin Carmo <kelvin.ocb@gmail.com>
2020-06-19 23:21:27 -0300

    Create encrypted_cesar.js

---
+++ functions/encrypted_cesar.js
@@ -0,0 +1,25 @@
+function getSum(asc_code, sum) {
+	let sum_real = sum % 26 == 0 ? sum : sum % 26;
+	let tot = asc_code + sum_real;
+
+	return tot > 122 ? tot - 26 : tot;
+}
+
+function encrypt(input, uni_sum) {
+	input = input.toLowerCase();
+	let newString = '';
+
+	for(let i = 0; i < input.length; i++) {
+		let asc_code = input.charCodeAt(i);
+
+		if(asc_code >= 97 && asc_code <= 122) 
+			newString += String.fromCharCode(getSum(asc_code, uni_sum));
+		else 
+			newString += input.charAt(i);
+	}
+
+	return newString;
+}
+
+console.log("DECODED", "abcz");
+console.log("ENCODED", encrypt("abcz", 3));
